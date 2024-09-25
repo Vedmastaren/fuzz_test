e827aed4b30034039d68d80fc4efef631fb89994
vedmastaren <simonbigdeli13@gmail.com>
2024-09-25 10:13:16 +0200

    added cmath lib to try

--- Builds/build_4/main/main.cpp
+++ Builds/build_4/main/main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include "math_lib/math.h" // Inkludera biblioteket
+#include <cmath> // Inkludera c++ egna math lib
 
 int main() {
     int a = 5;
@@ -10,5 +11,8 @@ int main() {
     std::cout << "Addition: " << add(a, b) << std::endl;
     std::cout << "Multiplikation: " << multiply(a, b) << std::endl;
 
+    // Användning av cmath
+    std::cout << "Kvadratroten av " << a << " är " << sqrt(a) << std::endl; 
+    std::cout << "Differensen av " << fsub(a, b) << std::endl;
     return 0;
 }
